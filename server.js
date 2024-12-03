d85622d75ea2a16f4c35572b2b7f26092e73f94d
MicheleSaraceni <michele.saraceni23@email.com>
2024-12-03 16:24:17 +0100

    Created public folder, and configured express to expose it

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 // importo express
 const express = require('express');
-const app = express();
+const server = express();
 
 // setto la porta
 const PORT = 3000;
 
-app.use(express.static("public"));
+server.use(express.static("public"));
 
-const pets = require("./images");
-console.log(pets);
-
-app.get("/", (req, res) => {
-    res.send("<h1>Qui inserisco del testo<h1>");
+server.get("/", (req, res) => {
+    res.send("<h1>Server del mio Blog<h1>");
 });
 
 
 
-app.all('*', (req, res) => {
+server.all('*', (req, res) => {
     res.status(404).send('<h1>Not Found !</h1>');
 })
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}}`);
 });
