e68b2085bbf1ed893fd84677583c5d79d8a98363
MicheleSaraceni <michele.saraceni23@email.com>
2024-12-03 18:31:51 +0100

    Ended basic exercise

---
+++ db/posts.js
@@ -0,0 +1,39 @@
+const posts = [
+    {
+        id: 1,
+        title: 'Post1',
+        content: 'aaaaaaaaaaa',
+        img: '',
+        tags: ['tag1', 'tag2', 'tag3']
+    },
+    {
+        id: 2,
+        title: 'Post2',
+        content: 'bbbbbbbbbbbbb',
+        img: '',
+        tags: ['tag4', 'tag2', 'tag5']
+    },
+    {
+        id: 3,
+        title: 'Post3',
+        content: 'cccccccccccc',
+        img: '',
+        tags: ['tag2', 'tag6', 'tag8']
+    },
+    {
+        id: 4,
+        title: 'Post4',
+        content: 'dddddddddddd',
+        img: '',
+        tags: ['tag3', 'tag5', 'tag6']
+    },
+    {
+        id: 5,
+        title: 'Post5',
+        content: 'eeeeeeeeeeeeee',
+        img: '',
+        tags: ['tag4', 'tag7', 'tag9']
+    }
+];
+
+module.exports = posts;
--- server.js
+++ server.js
@@ -2,16 +2,34 @@
 const express = require('express');
 const server = express();
 
+//importo posts.js
+const posts = require('./db/posts.js')
+
 // setto la porta
 const PORT = 3000;
 
+// console.log(posts);
+
 server.use(express.static("public"));
 
+// specializzo il server a rispondere alla richiesta GET
 server.get("/", (req, res) => {
-    res.send("<h1>Server del mio Blog<h1>");
+    res.send("<h1>Server del mio Blog</h1>");
 });
 
+// query - bacheca?tag=dolci&filter=100&start=10
+// params - bacheca/dolci/100/10
 
+server.get("/bacheca", (req, res) => {
+    console.log(res.posts);
+
+    const responseObject = {
+        lunghezza: posts.length,
+        posts
+    }
+
+    res.json(responseObject);
+});
 
 server.all('*', (req, res) => {
     res.status(404).send('<h1>Not Found !</h1>');
